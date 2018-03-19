0ccb36080ce4bfb6c227d708ce28809ca453642d
George V Jose <JORUJOSE@GMAIL.COM>
2018-03-19 16:26:23 +0530

    Update read_mem.c

--- S6 NPL Lab/read_mem.c
+++ S6 NPL Lab/read_mem.c
@@ -4,11 +4,10 @@
 #include <stdio.h>
 
 void main(){
-    	key_t key = 1234;
-    	int shmid = shmget(key,1024,0666|IPC_CREAT);
-    	char *str = (char*) shmat(shmid,(void*)0,0);
-    	printf("Enter String : ");
-	    gets(str);
-	    shmdt(str);
-	    printf("Write Complete\n");
+    key_t key = 1234;
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    char *str = (char*) shmat(shmid,(void*)0,0);
+    printf("Data in Shared Memory : %s\n",str);
+    shmdt(str);
+    shmctl(shmid,IPC_RMID,NULL);
 }
